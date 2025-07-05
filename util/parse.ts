0660e9fec94024f8aaccddd1323592473f8f03dd
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-05 10:37:13 -0700

    remove debugging logs

--- util/parse.ts
+++ util/parse.ts
@@ -46,7 +46,6 @@ function getSettingsFromFrontMatter(
   defaultSettings?: Record<string, unknown>,
 ): Settings {
   const frontMatter = parseFrontMatter(text, defaultSettings);
-  console.log("frontMatter: ", frontMatter.model);
   return SettingsSchema.parse(frontMatter);
 }
 
@@ -145,7 +144,6 @@ export function parseChatLogFromText(
   messages: { role: ChatRole; content: string }[];
 } {
   const settings = getSettingsFromFrontMatter(text, defaultSettings);
-  console.log('parseChatLog', settings.model);
   const chatLogText = stripFrontMatter(text);
   let messages = parseChatLog(chatLogText, settings);
   messages = [...defaultChatLog, ...messages];
