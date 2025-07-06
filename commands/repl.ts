04d3a0e422998fcaa0d87cc4e37561cf1e3932d5
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-06 04:58:13 -0700

    buffer output by default

--- commands/repl.ts
+++ commands/repl.ts
@@ -2,6 +2,10 @@ import repl from "repl";
 import { generateChatCompletionStream } from "../util/ai.js";
 import { parseGlobalChatConfig } from "../util/config.js";
 // import { parseChatLogFromText } from "../util/parse.js";
+import ora, { Ora } from "ora";
+
+const BUFFER_OUTPUT = true;
+const COLOR_OUTPUT = true;
 
 export async function handleRepl(input: string, opts: { file: string }) {
   const globalChatConfig = await parseGlobalChatConfig();
@@ -30,9 +34,21 @@ export async function handleRepl(input: string, opts: { file: string }) {
         });
         let reply = "";
         process.stdout.write(`${assistantCliPrompt}`);
+        let spinner: Ora | undefined;
+        if (BUFFER_OUTPUT) {
+          spinner = ora("Buffering input...").start();
+        }
         for await (const c of stream) {
-          process.stdout.write(c);
+          if (!BUFFER_OUTPUT) {
+            process.stdout.write(c);
+          }
           reply += c;
+        }
+        if (BUFFER_OUTPUT) {
+          if (spinner) {
+            spinner.stop();
+          }
+          process.stdout.write(reply);
         }
         process.stdout.write("\n");
         messages.push({ role: "assistant", content: reply });
