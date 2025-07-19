ddd9bd3259b13f4e00cf2b888d71d332e1ddeaf5
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-19 04:08:26 -0700

    v0.3.6

--- program.ts
+++ program.ts
@@ -14,7 +14,7 @@ const program = new Command();
 program
   .name("chatvim")
   .description("Chatvim: LLM-powered coding assistant")
-  .version("0.3.5")
+  .version("0.3.6")
   .action(commandMain);
 
 program
